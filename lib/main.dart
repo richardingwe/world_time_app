0dd253e0f7524356e005da26a8ebf19290a66e4a
richard ingwe <richardingwe5@gmail.com>
2021-12-21 21:16:52 +0100

    learned stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('World Time App!'),
-      ),),
+    home: Home()
   ));
+}
+
+class Home extends StatelessWidget {
+  // const ({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chinonso!'),
+        backgroundColor: Colors.green[900],
+      ),
+      body: Center(
+        child: Text('Hello, Richard Loves Nonso!',
+        style: TextStyle(
+          fontSize: 22.0,
+          color: Colors.green[900]
+        ),),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('+'),
+        backgroundColor: Colors.green[900],
+        hoverColor: Colors.purple[900],
+      ),
+    );
+  }
 }
