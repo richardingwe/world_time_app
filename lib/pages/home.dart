28b5db815a6d5dd7d8ec2243fd7fa1220b370693
richard ingwe <richardingwe5@gmail.com>
2021-12-24 08:03:32 +0100

    wrapped home with safe area widget.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,7 +6,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('home'),
+      body: SafeArea(child: Text('home')),
     );
   }
 }
