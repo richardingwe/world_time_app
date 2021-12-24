aca653ef3efcc8f93a7dbe4df4621ffbd475c855
richard ingwe <richardingwe5@gmail.com>
2021-12-24 08:07:54 +0100

    created the loading widget

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,1 +1,20 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+
+
+class Loading extends StatefulWidget {
+  const Loading({ Key? key }) : super(key: key);
+
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Loading Screen'),
+    );
+  }
+}
