df4d1d5e83d5bdd9c8a8e61742123c96363f9bdd
richard ingwe <richardingwe5@gmail.com>
2021-12-26 06:05:43 +0100

    added world_time class

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -11,25 +11,7 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getData() async {
-// make the request
-    print('hello');
-    var url = Uri.parse('http://worldtimeapi.org/api/timezone/Europe/London');
-    Response response = await get(url);
-    Map data = jsonDecode(response.body);
-    print(data);
-
-    // get properties from json
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    //print(datetime);
-    //print(offset);
-
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
-  }
+  void getData() async {}
 
   @override
   void initState() {
